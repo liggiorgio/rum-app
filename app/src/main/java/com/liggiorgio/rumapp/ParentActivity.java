90b785b2099938dfe0731da78275751878a3827f
liggiorgio <liggiorgio@live.com>
2018-12-06 17:31:32 +0100

    Add Toolbar action bar

---
+++ app/src/main/java/com/liggiorgio/rumapp/ParentActivity.java
@@ -0,0 +1,20 @@
+package com.liggiorgio.rumapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class ParentActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}
--- app/src/main/java/com/liggiorgio/rumapp/SplashActivity.java
+++ app/src/main/java/com/liggiorgio/rumapp/SplashActivity.java
@@ -1,5 +1,11 @@
+/*
+    This activity shows a splash screen to the user as long as the Android system
+    is setting up the app, then moves to the main activity without wasting time.
+*/
+
 package com.liggiorgio.rumapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +15,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // Start the main activity
+        Intent intent = new Intent(this, ParentActivity.class);
+        startActivity(intent);
+        finish();
     }
 }

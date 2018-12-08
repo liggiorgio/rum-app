1f2c0f57a278fc85075ab1ad63dc9f58e3c50676
liggiorgio <liggiorgio@live.com>
2018-12-08 19:46:17 +0100

    Remove splash screen layout

--- app/src/main/java/com/liggiorgio/rumapp/SplashActivity.java
+++ app/src/main/java/com/liggiorgio/rumapp/SplashActivity.java
@@ -15,7 +15,6 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
         // Start the main activity
         Intent intent = new Intent(this, NewsActivity.class);

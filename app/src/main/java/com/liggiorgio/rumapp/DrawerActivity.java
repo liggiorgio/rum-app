424a192e5c85334723d158eabbb67d69ff2a6a20
liggiorgio <liggiorgio@live.com>
2018-12-14 11:35:28 +0100

    Add up and back navigation

--- app/src/main/java/com/liggiorgio/rumapp/DrawerActivity.java
+++ app/src/main/java/com/liggiorgio/rumapp/DrawerActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.NavUtils;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -32,92 +33,112 @@ public abstract class DrawerActivity extends ParentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Highlight the corresponding item in the navigation drawer
-        ((NavigationView) findViewById(R.id.navigation_view)).getMenu().getItem(getMenuItemId()).setChecked(true);
-
         // Inflate child layout in parent
         LinearLayout contentLayout = findViewById(R.id.content_layout);
         getLayoutInflater().inflate(childLayoutId, contentLayout);
 
-        // Enable home button
+        // Enable home/up buttons
         ActionBar actionbar = getSupportActionBar();
         assert actionbar != null;
         actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        if (getMenuItemId() == 0)
+            actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
         // Handle navigation click events
         drawerLayout = findViewById(R.id.drawer_layout);
 
         NavigationView navigationView = findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                        // Set item as selected to persist highlight
-                        menuItem.setChecked(true);
-                        // Close drawer when item is tapped
-                        drawerLayout.closeDrawers();
+            new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    // Set item as selected to persist highlight
+                    menuItem.setChecked(true);
+                    // Close drawer when item is tapped
+                    drawerLayout.closeDrawers();
 
-                        // TODO: Add code here to update the UI based on the item selected
-                        // For example, swap UI fragments here
-                        switch (menuItem.getItemId()) {
-                            case R.id.action_home: {
-                                // Go to home activity
-                                Intent intent = new Intent(getApplicationContext(), NewsActivity.class);
-                                startActivity(intent);
-                                finish();
-                            } break;
-                            case R.id.action_categories: {
-                                // Go to categories activity
-                                Intent intent = new Intent(getApplicationContext(), CategoriesActivity.class);
-                                startActivity(intent);
-                                finish();
-                            } break;
-                            case R.id.action_about: {
-                                // Go to about activity
-                                Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
-                                startActivity(intent);
-                                finish();
-                            } break;
-                            case R.id.action_contacts: {
-                                // Go to contacts activity
-                                Intent intent = new Intent(getApplicationContext(), ContactsActivity.class);
-                                startActivity(intent);
-                                finish();
-                            } break;
-                            case R.id.action_places: {
-                                // Go to places activity
-                                Intent intent = new Intent(getApplicationContext(), PlacesActivity.class);
-                                startActivity(intent);
-                                finish();
-                            } break;
-                            case R.id.action_help: {
-                                // Go to FAQ activity
-                                Intent intent = new Intent(getApplicationContext(), HelpActivity.class);
-                                startActivity(intent);
-                                finish();
-                            } break;
-                            case R.id.action_info: {
-                                // Go to info activity
-                                Intent intent = new Intent(getApplicationContext(), InfoActivity.class);
-                                startActivity(intent);
-                                finish();
-                            } break;
-                            default: break;
-                        }
-
-                        return true;
+                    // TODO: Add code here to update the UI based on the item selected
+                    // For example, swap UI fragments here
+                    switch (menuItem.getItemId()) {
+                        case R.id.action_home: {
+                            // Go to home activity
+                            Intent intent = new Intent(getApplicationContext(), NewsActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } break;
+                        case R.id.action_categories: {
+                            // Go to categories activity
+                            Intent intent = new Intent(getApplicationContext(), CategoriesActivity.class);
+                            startActivity(intent);
+                            //finish();
+                        } break;
+                        case R.id.action_about: {
+                            // Go to about activity
+                            Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+                            startActivity(intent);
+                            //finish();
+                        } break;
+                        case R.id.action_contacts: {
+                            // Go to contacts activity
+                            Intent intent = new Intent(getApplicationContext(), ContactsActivity.class);
+                            startActivity(intent);
+                            //finish();
+                        } break;
+                        case R.id.action_places: {
+                            // Go to places activity
+                            Intent intent = new Intent(getApplicationContext(), PlacesActivity.class);
+                            startActivity(intent);
+                            //finish();
+                        } break;
+                        case R.id.action_help: {
+                            // Go to FAQ activity
+                            Intent intent = new Intent(getApplicationContext(), HelpActivity.class);
+                            startActivity(intent);
+                            //finish();
+                        } break;
+                        case R.id.action_info: {
+                            // Go to info activity
+                            Intent intent = new Intent(getApplicationContext(), InfoActivity.class);
+                            startActivity(intent);
+                            //finish();
+                        } break;
+                        default: break;
                     }
-                });
+                    return true;
+                }
+            });
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Highlight the corresponding item in the navigation drawer
+        ((NavigationView) findViewById(R.id.navigation_view)).getMenu().getItem(getMenuItemId()).setChecked(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawers();
+            return;
+        }
+
+        // Otherwise defer to system default behavior.
+        super.onBackPressed();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
+            case android.R.id.home: {
+                if (getMenuItemId() == 0) {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                    return true;
+                }
+                NavUtils.navigateUpFromSameTask(this);
                 return true;
+            }
         }
         return super.onOptionsItemSelected(item);
     }

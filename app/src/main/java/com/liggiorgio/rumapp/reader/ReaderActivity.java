2814cd98ad807280ae1440c7cdcbb5579afd79f3
liggiorgio <liggiorgio@live.com>
2018-12-30 00:28:07 +0100

    Fix deprecated method on API >=24

--- app/src/main/java/com/liggiorgio/rumapp/reader/ReaderActivity.java
+++ app/src/main/java/com/liggiorgio/rumapp/reader/ReaderActivity.java
@@ -2,6 +2,7 @@ package com.liggiorgio.rumapp.reader;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.text.Html;
@@ -50,12 +51,17 @@ public class ReaderActivity extends ParentActivity {
         });
     }
 
+    @SuppressWarnings("deprecation")
     private void updateView() {
         // Load text from web page
         ((TextView) findViewById(R.id.reader_title)).setText(article.getTitle());
         ((TextView) findViewById(R.id.reader_author)).setText(article.getAuthor());
         ((TextView) findViewById(R.id.reader_categories)).setText(article.getCategories());
-        ((TextView) findViewById(R.id.reader_text)).setText(Html.fromHtml(article.getText()));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            ((TextView) findViewById(R.id.reader_text)).setText(Html.fromHtml(article.getText(), Html.FROM_HTML_MODE_LEGACY));
+        } else {
+            ((TextView) findViewById(R.id.reader_text)).setText(Html.fromHtml(article.getText()));
+        }
         (findViewById(R.id.reader_text)).setClickable(true);
         ((TextView) findViewById(R.id.reader_text)).setMovementMethod(LinkMovementMethod.getInstance());
     }

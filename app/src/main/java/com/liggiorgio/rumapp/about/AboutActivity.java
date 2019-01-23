02da63b1de2da04a315bdc1a40e367c03620af28
liggiorgio <liggiorgio@live.com>
2019-01-23 23:04:28 +0100

    Populate abouts activity with data

--- app/src/main/java/com/liggiorgio/rumapp/about/AboutActivity.java
+++ app/src/main/java/com/liggiorgio/rumapp/about/AboutActivity.java
@@ -1,6 +1,8 @@
 package com.liggiorgio.rumapp.about;
 
 import android.os.Bundle;
+import android.support.v7.content.res.AppCompatResources;
+import android.widget.Button;
 import com.liggiorgio.rumapp.DrawerActivity;
 import com.liggiorgio.rumapp.R;
 
@@ -9,7 +11,6 @@ public class AboutActivity extends DrawerActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
     }
 

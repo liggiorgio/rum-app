1484ac9aa36c4cc272d977f8474184f06041cc15
liggiorgio <liggiorgio@live.com>
2019-02-05 17:56:00 +0100

    Add browsable categories

--- app/src/main/java/com/liggiorgio/rumapp/cats/CategoriesViewModel.java
+++ app/src/main/java/com/liggiorgio/rumapp/cats/CategoriesViewModel.java
@@ -22,11 +22,19 @@ public class CategoriesViewModel extends ViewModel {
     // Application categories
     private void loadCats() {
         ArrayList<CategoriesItem> cats = new ArrayList<>();
-        for (int i=0; i<5; i++) {
-            cats.add(new CategoriesItem(R.drawable.ic_about_star, "Ultime notizie", null));
-            cats.add(new CategoriesItem(R.drawable.ic_about_compass, "Orientamento didattico", null));
-            cats.add(new CategoriesItem(R.drawable.ic_about_contact, "Segreterie", null));
-        }
+        //cats.add(new CategoriesItem(0, "", null));
+        cats.add(new CategoriesItem(R.drawable.ic_topic_science, "Scienze di base e applicate", null));
+        cats.add(new CategoriesItem(R.drawable.ic_topic_engineering, "Politecnica", null));
+        cats.add(new CategoriesItem(R.drawable.ic_topic_law, "Scienze giuridiche ed economico-sociali", null));
+        cats.add(new CategoriesItem(R.drawable.ic_topic_humanities, "Scienze umane e del patrimonio culturale", null));
+        cats.add(new CategoriesItem(R.drawable.ic_topic_medicine, "Scuola di Medicina e Chirurgia", null));
+        cats.add(new CategoriesItem(R.drawable.ic_topic_school, "Ateneo", null));
+        cats.add(new CategoriesItem(R.drawable.ic_topic_erasmus, "Erasmus", null));
+        cats.add(new CategoriesItem(R.drawable.ic_topic_management, "E.R.S.U.", null));
+        cats.add(new CategoriesItem(R.drawable.ic_topic_management, "Consiglio di Amministrazione", null));
+        cats.add(new CategoriesItem(R.drawable.ic_topic_school, "Senato accademico", null));
+        cats.add(new CategoriesItem(R.drawable.ic_topic_sport, "C.U.S.", null));
+        cats.add(new CategoriesItem(R.drawable.ic_topic_exam, "Test di ammissione", null));
         allCats.setValue(cats);
     }
 }
